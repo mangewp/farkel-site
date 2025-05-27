1ce889bc7d0749abb6dfffda123181b8c7ad75dd
mangewp <114552742+mangewp@users.noreply.github.com>
2025-05-26 22:10:37 -0500

    removed imports

---
+++ src/components/LeftColumn.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import SideBarButton from './SideBarButton';
+import Album from './Album';
+import './LeftColumn.css';
+
+export default function LeftColumn() {
+  return (
+    <div className="home-left">
+      <h1 style={{ margin: 0, marginBottom: '2rem', fontSize: '2.2rem' }}>
+        Farkel
+      </h1>
+      <SideBarButton href="https://www.youtube.com/@farkelz">YouTube</SideBarButton>
+      <SideBarButton href="https://open.spotify.com/artist/0F0N3RSvTkGppo8OYcGMBe">Spotify</SideBarButton>
+      <SideBarButton href="https://music.apple.com/au/artist/farkel/1728006176?at=1000l3auP&ct=link&itsct=music_box_link&itscg=30200&ls=1">Apple Music</SideBarButton>
+      <SideBarButton href="https://www.instagram.com/farkelz">Instagram</SideBarButton>
+      <h1 style={{ marginTop: 20, marginBottom: '2rem', fontSize: '2.2rem', }}>
+        Albums
+      </h1>
+      <div className="albums-list">
+        <Album
+          image="src/assets/STAY UGLY.jpg"
+          button1={{ text: 'Spotify', link: 'https://open.spotify.com/' }}
+          button2={{ text: 'Apple', link: 'https://music.apple.com/' }}
+        />
+        <Album
+          image="src/assets/HURRICANE HEART.jpg"
+          button1={{ text: 'Spotify', link: 'https://open.spotify.com/' }}
+          button2={{ text: 'Apple', link: 'https://music.apple.com/' }}
+        />
+        <Album
+          image="src/assets/FAR FROM GONE.jpg"
+          button1={{ text: 'Spotify', link: 'https://open.spotify.com/' }}
+          button2={{ text: 'Apple', link: 'https://music.apple.com/' }}
+        />
+      </div>
+    </div>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,41 +1,12 @@
 import React from 'react';
 import './Home.css';
-import SideBarButton from '../components/SideBarButton';
-import Album from '../components/Album';
+import LeftColumn from '../components/LeftColumn';
 
 function Home() {
   return (
     <div className="home-root">
       {/* Left Section */}
-      <div className="home-left">
-        <h1 style={{ margin: 0, marginBottom: '2rem', fontSize: '2.2rem', letterSpacing: '2px' }}>
-          Farkel
-        </h1>
-        <SideBarButton href="https://www.youtube.com/@farkelz">YouTube</SideBarButton>
-        <SideBarButton href="https://open.spotify.com/artist/0F0N3RSvTkGppo8OYcGMBe">Spotify</SideBarButton>
-        <SideBarButton href="https://music.apple.com/au/artist/farkel/1728006176?at=1000l3auP&ct=link&itsct=music_box_link&itscg=30200&ls=1">Apple Music</SideBarButton>
-        <SideBarButton href="https://www.instagram.com/farkelz">Instagram</SideBarButton>
-        <h1 style={{ marginTop: '10rem', marginBottom: '2rem', fontSize: '2.2rem', letterSpacing: '2px' }}>
-          Albums
-        </h1>
-        <div className="albums-list">
-          <Album
-            image="src/assets/STAY UGLY.jpg"
-            button1={{ text: 'Spotify', link: 'https://open.spotify.com/' }}
-            button2={{ text: 'Apple', link: 'https://music.apple.com/' }}
-          />
-          <Album
-            image="src/assets/HURRICANE HEART.jpg"
-            button1={{ text: 'Spotify', link: 'https://open.spotify.com/' }}
-            button2={{ text: 'Apple', link: 'https://music.apple.com/' }}
-          />
-          <Album
-            image="src/assets/FAR FROM GONE.jpg"
-            button1={{ text: 'Spotify', link: 'https://open.spotify.com/' }}
-            button2={{ text: 'Apple', link: 'https://music.apple.com/' }}
-          />
-</div>
-      </div>
+      <LeftColumn />
 
       {/* Right Section */}
       <div className="home-right">
@@ -71,10 +42,10 @@ function Home() {
             gap: '1.5rem',
             width: '90%',
             margin: '0 auto',
-            marginBottom: '5rem',
+            marginBottom: '1rem',
           }}
         >
-          {[2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16].map((index) => (
+          {[2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13].map((index) => (
             <div
               key={index}
               style={{
@@ -84,6 +55,7 @@ function Home() {
                 borderRadius: '12px',
                 overflow: 'hidden',
                 boxShadow: '0 2px 16px rgba(0,0,0,0.18)',
+                paddingBottom: '5rem',
               }}
             >
               <iframe
